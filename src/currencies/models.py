be3e140d029d36e7eb92c21a410801847a33d5ea
Stefano Rivera <stefanor@debian.org>
2022-09-23 11:12:58 +0200

    Initial models

---
+++ src/currencies/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class ExchangeRate(models.Model):
+    date = models.DateField()
+    currency = models.CharField(max_length=3)
+    base_currency = models.CharField(max_length=3)
+    rate = models.DecimalField(decimal_places=5, max_digits=15)
+
+    class Meta:
+        unique_together = [
+            ["date", "currency", "base_currency"],
+        ]
