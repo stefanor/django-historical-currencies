c04d6bb739d89c30a212ff7b74a821f9ef8b40e3
Stefano Rivera <stefanor@debian.org>
2023-04-15 16:02:39 -0400

    Use the Django checks framework to check for common deployment mistakes

--- src/currencies/models.py
+++ src/currencies/models.py
@@ -1,3 +1,7 @@
+from datetime import date, timedelta
+
+from django.conf import settings
+from django.core.checks import Error, Tags, register
 from django.db import models
 
 
@@ -14,3 +18,22 @@ class ExchangeRate(models.Model):
 
     def __str__(self):
         return f"Exchange Rate: {self.base_currency}-{self.currency} @ {self.date}: {self.rate}"
+
+
+@register(Tags.database, deploy=True)
+def check_fresh_exchange_rate_data(app_configs, **kwargs):
+    errors = []
+    oldest_acceptable_rate = date.today() - timedelta(
+        days=settings.MAX_EXCHANGE_RATE_AGE
+    )
+    rates = ExchangeRate.objects.filter(date__gte=oldest_acceptable_rate)
+    if not rates.exists():
+        errors.append(
+            Error(
+                "no current exchange rates",
+                hint="Run manage.py import_ecb_exchangerates to populate exchange rates",
+                id="currencies.E001",
+            )
+        )
+
+    return errors
