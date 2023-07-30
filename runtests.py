002c3e8a55c8a4876f6068ae96ef56c046006f0c
Stefano Rivera <stefano@rivera.za.net>
2023-07-30 18:21:45 +0200

    Skip tests that require internet by default

--- runtests.py
+++ runtests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import os
 import sys
 
@@ -6,10 +7,27 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument(
+        "--internet",
+        action="store_true",
+        help="Run tests that make requests on the Internet",
+    )
+    args = p.parse_args()
+
+    exclude_tags = []
+    if not args.internet:
+        exclude_tags.append("internet")
+
     os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner()
+    test_runner = TestRunner(exclude_tags=exclude_tags)
     failures = test_runner.run_tests(["historical_currencies"])
     sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    main()
