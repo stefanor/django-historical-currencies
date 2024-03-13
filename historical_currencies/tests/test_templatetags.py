298df1ebf081c4b0a419a277255b2166c2c7b5cb
Stefano Rivera <stefano@rivera.za.net>
2024-03-13 12:31:39 -0400

    Refactor rendering in templatetag tests

--- historical_currencies/tests/test_templatetags.py
+++ historical_currencies/tests/test_templatetags.py
@@ -7,29 +7,31 @@ from django.test import SimpleTestCase, TestCase
 from historical_currencies.models import ExchangeRate
 
 
-def render_template(body, context, load_currency_format=True):
-    if isinstance(context, dict):
-        context = Context(context)
-    if load_currency_format:
-        body = "{% load currency_format %}" + body
-    return Template(body).render(context)
-
-
 class CurrencyTagTestCase(SimpleTestCase):
+    def render(self, body, context):
+        if isinstance(context, dict):
+            context = Context(context)
+        body = "{% load currency_format %}" + body
+        return Template(body).render(context)
+
     def test_currency_filter(self):
-        rendered = render_template("{{ ben|currency }}", {"ben": (Decimal(100), "USD")})
+        rendered = self.render("{{ ben|currency }}", {"ben": (Decimal(100), "USD")})
         self.assertEqual(rendered, "100.00 USD")
 
 
 class ExchangeRateTagTestCase(TestCase):
+    def render(self, body, context):
+        if isinstance(context, dict):
+            context = Context(context)
+        body = "{% load currency_format %}" + body
+        return Template(body).render(context)
+
     def test_exchange_filter_with_rate(self):
         date = datetime.date.today()
         ExchangeRate.objects.create(
             date=date, base_currency="EUR", currency="USD", rate=1.1326
         )
-        rendered = render_template(
-            "{{ eur|exchange:'USD' }}", {"eur": (Decimal(1), "EUR")}
-        )
+        rendered = self.render("{{ eur|exchange:'USD' }}", {"eur": (Decimal(1), "EUR")})
         self.assertEqual(rendered, "1.13 USD")
 
     def test_exchange_filter_missing_rate(self):
@@ -37,7 +39,7 @@ class ExchangeRateTagTestCase(TestCase):
             "historical_currencies.templatetags.currency_format",
             level="WARNING",
         ) as cm:
-            rendered = render_template(
+            rendered = self.render(
                 "{{ eur|exchange:'USD' }}", {"eur": (Decimal(1), "EUR")}
             )
         self.assertEqual(rendered, "? (no rate) USD")
